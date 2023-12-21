8316ad69fcf3f04cd55983becfdcb26e5c04d023
Gonçalo Raposeiro <goncalo.raposeiro@outlook.com>
2023-12-21 03:46:33 +0000

    Adding icons
    Signed-off-by: Gonçalo Raposeiro <goncalo.raposeiro@outlook.com>

--- components/TaskItem.tsx
+++ components/TaskItem.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import type { Task } from '../types';
 import {SVGComponent} from "./ui/SVG";
+import {formatDate, isDateNow} from "../utils";
 
 type TaskItemProps = {
     task: Task;
@@ -10,7 +11,6 @@ type TaskItemProps = {
 };
 
 const TaskItem: React.FC<TaskItemProps> = ({ task, onComplete, onClick, onDelete  }) => {
-
     return (
         <div className="taskItem" onClick={() => onClick(task.id)}>
             <div className="checkboxWrapper">
@@ -34,19 +34,24 @@ const TaskItem: React.FC<TaskItemProps> = ({ task, onComplete, onClick, onDelete
                     <div className="taskInfo">
                         {task.repeat && (
                             <div className="taskInfoDetails">
-                                <SVGComponent icon={'creditCard'} size={12}/>
-                                {task.repeat}
+                                <SVGComponent icon={'repeat'} size={12}/>
+                                <p>{task.repeat}</p>
                             </div>
                         )}
                         {task.date && (
                             <div className="taskInfoDetails">
-                                <SVGComponent icon={'chart'} size={12}/>
-                                {task.date}
+                                <SVGComponent icon={'calendar'} size={12}/>
+                                <p style={
+                                    {
+                                        color: task.date && isDateNow(task.date) ? "#cc2626" : "#c2c2c2",
+                                        fontWeight: task.date && isDateNow(task.date) ? "bold" : "normal"
+                                    }}
+                                >{task.date}</p>
                             </div>
                         )}
                         {task.note && (
                             <div className="taskInfoDetails">
-                                <SVGComponent icon={'person'} size={12}/>
+                                <SVGComponent icon={'note'} size={12}/>
                             </div>
                         )}
                     </div>
--- utils/index.ts
+++ utils/index.ts
@@ -8,3 +8,7 @@ export const formatDate = (dateString?: string) => {
     }
     return new Date(dateString).toISOString().split('T')[0];
 };
+
+export const isDateNow = (date: string): boolean => {
+    return date === new Date().toISOString().split('T')[0];
+}
